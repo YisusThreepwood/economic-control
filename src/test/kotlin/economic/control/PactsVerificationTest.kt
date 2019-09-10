b96bfd3d265bfc0a74e4d7a1b8fb6688efdbb5f8
Jesus Hernando Sancha <jesushs80@gmail.com>
2019-09-10 18:07:23 +0200

    Add Consumer Drive Contract verification

---
+++ src/test/kotlin/economic/control/PactsVerificationTest.kt
@@ -0,0 +1,38 @@
+package economic.control
+
+import au.com.dius.pact.provider.junit.Provider
+import au.com.dius.pact.provider.junit.State
+import au.com.dius.pact.provider.junit.loader.PactFolder
+import au.com.dius.pact.provider.junit.target.TestTarget
+import au.com.dius.pact.provider.junit5.HttpTestTarget
+import au.com.dius.pact.provider.junit5.PactVerificationContext
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider
+import io.micronaut.test.annotation.MicronautTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.TestTemplate
+import org.junit.jupiter.api.extension.ExtendWith
+import java.net.URL
+
+@MicronautTest
+@Provider("EconomicControl")
+@PactFolder("pacts")
+class PactsVerificationTest {
+
+    /*@TestTarget
+    val target: HttpTestTarget = HttpTestTarget("localhost", 8090)*/
+
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider::class)
+    fun pactsVerificationTemplate(context: PactVerificationContext) {
+        context.verifyInteraction()
+    }
+
+    @BeforeEach
+    fun setupContext(context: PactVerificationContext) {
+        context.target = HttpTestTarget.fromUrl(URL("http://localhost:8090"))
+    }
+
+    @State("Get allowed amount with decimals for an existing club")
+    fun testGetAllowedAmountWithDecimal() {
+    }
+}
