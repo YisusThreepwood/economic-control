95e789c1760779ac36302ca54b76d35df6806e43
Jesus Hernando Sancha <jesushs80@gmail.com>
2019-10-08 13:24:07 +0200

    Modify PlayerHiring pact by checking request and response headers

--- src/main/kotlin/economic/control/balance/BalanceController.kt
+++ src/main/kotlin/economic/control/balance/BalanceController.kt
@@ -10,6 +10,7 @@ class BalanceController @Inject
 constructor(private val balanceInteractor: BalanceInteractor, private val balanceTransformer: BalanceTransformer){
 
     @Get("/{clubId}")
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     fun show(clubId: String): HttpResponse<Any?> {
         return  HttpResponse.ok(
